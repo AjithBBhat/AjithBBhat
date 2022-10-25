9787847b37e3a4d60d6bf202d345504e1932a9c1
AjithBBhat <ajithbbhat@gmail.com>
2022-10-26 00:10:52 +0530

    init

---
+++ CRUD/server/routes/router.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const route = express.Router()
+
+
+const services = require('../services/render');
+
+/**
+ *@description Root route
+ @method GET 
+ */ 
+route.get('/',services.homeRoutes);
+
+/**
+ *@description add-user route
+ @method GET /add-user
+ */ 
+ route.get('/add-user',services.add_user);
+
+ /**
+ *@description update-user route
+ @method GET /update-user
+ */ 
+ route.get('/update-user',services.update_user);
+
+
+module.exports = route
