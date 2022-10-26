8e35d8ac77858a9d209ae1bda0ec2b583a5ea434
AjithBBhat <ajithbbhat@gmail.com>
2022-10-27 00:18:30 +0530

    init

--- CRUD/server.js
+++ CRUD/server.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const bodyparser = require('body-parser');
+const bodyparser = require("body-parser");
 const path = require('path');
+
+const connectDB = require('./server/database/connection');
 
 const app = express();
 
-dotenv.config({path:'config.env'})
-const PORT = process.env.PORT || 8080 
+dotenv.config( { path : 'config.env'} )
+const PORT = process.env.PORT || 8080
 
-//log requests
+// log requests
 app.use(morgan('tiny'));
 
-//parser request to body-parser
-app.use(bodyparser.urlencoded({extended:true}))
+// mongodb connection
+connectDB();
 
-//set view engine
-app.set("view engine","ejs")
-//app.set("views",path.resolve(__dirname,"views/ejs"))
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
 
-//load assets
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
-// load router
-app.use('/',require('./server/routes/router'))
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.listen(PORT,()=>{console.log(`Server is running on  http://localhost:${PORT}`)});
+// load routers
+app.use('/', require('./server/routes/router'))
 
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
---
+++ CRUD/server/database/connection.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        mongoose.connect('mongodb+srv://admin:admin123@cluster0.mfbvo8v.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        // useFindAndModify: false,
+          //  useCreateIndex: true
+        });
+        mongoose.connection.once('open', function(){
+          console.log('Conection has been made!');
+        })
+
+      //  console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
